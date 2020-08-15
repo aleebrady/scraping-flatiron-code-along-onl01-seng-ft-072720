31f4eda1a5ca0e8020db3fefbff31df7e9826208
Aaron Brady <aleebrady@gmail.com>
2020-08-15 07:34:59 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,19 @@
 class Course
   
-  attr_accessor :title, :schedule, :description 
+  attr_accessor :title, :schedule, :description
+  
+  @@all = [] 
+  
+  def initialize
+    @@all << self
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
   
   
 end
